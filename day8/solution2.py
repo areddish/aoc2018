498935cd8e8065bf4f9441e66f25dcb4274a939d
Andrew Reddish <areddish@foosion.net>
2018-12-08 00:45:07 -0500

    Reverse list so to just pop and not deal with passing index around

--- day8/solution2.py
+++ day8/solution2.py
@@ -23,6 +23,7 @@ class Node:
 
         value = 0
         for x in self.metadata:
+            # 1-based -> 0-based
             index = x - 1
             if (index >=0 and index < len(self.children)):
                 value += self.children[index].get_value()
@@ -30,36 +31,33 @@ class Node:
         #print(self.name, value)
         return value
 
-def read_node(nodes, input_set, i):
+def read_node(nodes, input_set):
     node_index = len(nodes)
     node = Node(node_index)
     nodes[node_index] = node
     
-    num_child_nodes = input_set[i]
-    i+=1
-    num_metadata_entries = input_set[i]
-    i+=1
+    num_child_nodes = input_set.pop()
+    num_metadata_entries = input_set.pop()
 
     #print (node.name,"has",num_child_nodes,"and",num_metadata_entries)
 
     for x in range(num_child_nodes):
-        i, child = read_node(nodes, input_set, i)
-        node.add_child(child)
+        node.add_child(read_node(nodes, input_set))
 
     for x in range(num_metadata_entries):
-        node.add_metadata(input_set[i])
-        i+=1
+        node.add_metadata(input_set.pop())
 
-    return i, node
+    return node
 
 with open(WHICH[0], "rt") as file:
     data = file.readline().strip()
 
     input_set = [int(x) for x in data.split(' ')]
+    input_set.reverse()
     #print (input_set)
 
     nodes = collections.defaultdict(list)
-    i, node = read_node(nodes, input_set, 0)
+    root = read_node(nodes, input_set)
 
     print ("Part 1", "-"*50)
     running_sum = 0
@@ -68,4 +66,4 @@ with open(WHICH[0], "rt") as file:
     print(running_sum)
 
     print ("Part 2", "-"*50)
-    print (node.get_value())
+    print (root.get_value())
