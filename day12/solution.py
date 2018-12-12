78934971987c2a1106f1add84fd4042069e14418
Andrew Reddish <areddish@foosion.net>
2018-12-12 03:41:45 -0500

    Clean up

--- day12/solution.py
+++ day12/solution.py
@@ -1,3 +1,5 @@
+buffer = "......"
+
 def advance_generation(state, rules):
     next_gen = ['.'] * len(state)
 
@@ -8,15 +10,19 @@ def advance_generation(state, rules):
             next_gen[index+2] = rule[1]
             index = state.find(rule[0], index + 1)
 
-    # add room to grow, could probably be smaller
-    return "".join(next_gen) + "...."
+    # Add room to grow, could probably add a smaller buffer... If the rules grew differently
+    # then we'd potentially need to prepend a buffer too and track the 0th place for proper summing.
+    return "".join(next_gen) + buffer
 
 
 def plant_sum(state):
-    sum = 0
+    global buffer
+    # subtract off the length of the buffer because we added this to the initial state which moved the 0th place
+    buf_len = len(buffer)
+    result = 0
     for i in range(len(state)):
-        sum += (i-6) if state[i] == '#' else 0
-    return sum
+        result += (i-buf_len) if state[i] == '#' else 0
+    return result
 
 def no_change(deltas):
     for x in deltas[1:]:
@@ -28,7 +34,7 @@ def no_change(deltas):
 with open("input.txt", "rt") as file:
     data = file.readlines()
 
-    current_state = "......" + data[0].split(' ')[2].strip() + "....."
+    current_state = buffer + data[0].split(' ')[2].strip() + buffer
 
     rules = []
     for rule in data[2:]:
@@ -45,9 +51,8 @@ with open("input.txt", "rt") as file:
         next_generation = advance_generation(current_state, rules)
         current_generation += 1
 
-        sum = plant_sum(next_generation)
-
-        delta = sum - prev_sum
+        current_sum = plant_sum(next_generation)
+        delta = current_sum - prev_sum
 
         # inefficient, but works. should use deque
         prev_deltas.insert(0, delta)
@@ -56,11 +61,11 @@ with open("input.txt", "rt") as file:
         if (no_change(prev_deltas)):
             # we can solve now
             print(f"Solving after generation: {current_generation} with delta of {delta}")
-            print("Part 2 solution:",(generation_desired - current_generation)*delta + sum)
+            print("Part 2 solution:",(generation_desired - current_generation)*delta + current_sum)
             exit(0)
 
-        prev_sum = sum
+        prev_sum = current_sum
         current_state = next_generation
 
         if (current_generation == 20):
-            print(f"Part 1 solution: {plant_sum(current_state)}")
+            print(f"Part 1 solution: {current_sum}")
